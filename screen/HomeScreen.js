b1220bf65d257ead654420ff2060e81225e2391d
edu <cesconettoedu@gmail.com>
2023-04-25 23:40:49 -0400

    add gif ENTER

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,20 +1,53 @@
-import React from "react";
-import { View, Text, Button, StyleSheet, Image } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, Button, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { useIsFocused } from '@react-navigation/native'
 
 const HomeScreen = ({navigation}) => {
+
+  const [showLogo, setShowLogo] = useState(true);
+  const [btn, setBtn] = useState ('ENTER');
+  const isFocused = useIsFocused();
+
+  const press = () => {
+    setShowLogo(false);
+    setBtn('WELCOME')
+    setTimeout(() => {
+      navigation.navigate('ListBeerScreen')
+    },3000)
+  }
+  
+  useEffect(() => {
+    isFocused
+  },[isFocused]);
+
   return (
     <View style={styles.container}>
+
+      <Text
+        style={[styles.title, { display: showLogo ? 'flex' : 'none' }]}
+      > Ed's Beer List</Text>
       
       <Image 
+        style={[styles.myLogo, { display: showLogo ? 'flex' : 'none' }]}
+        // style={styles.myLogo}
+        source={require('../assets/eulogo.png')}
+      />
+      
+      <Image 
+        style={[styles.cheers, { display: showLogo ? 'none' : 'flex' }]}
+      //  style={styles.cheers}
         source={require('../assets/cheers.gif')}
-        style={{width: 320, height: 250}}
       />
 
-      <Text>Home Screen</Text>
-      <Button
-        title="ENTER"
-        onPress={() => navigation.navigate('ListBeerScreen')}
-      />
+      <TouchableOpacity 
+        style={[styles.enterContainer]}
+        title={btn}
+        onPress={press}
+      >
+        <Text style={[styles.enter, { display: showLogo ? 'flex' : 'none' }]}>ENTER</Text>
+      </TouchableOpacity>
+
+
     </View>
   )
 }
@@ -26,7 +59,31 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#8fcbbc'
+    // backgroundColor: '#8fcbbc'
+    backgroundColor: 'black'
+  },
+  title: {
+    color: '#69e9f5',
+    fontSize: 40,
+    fontWeight: 600
+  },
+  enterContainer: {
 
+  },
+  enter: {
+    color: '#69e9f5',
+    fontSize: 40,
+    fontWeight: 600,
+    borderRadius:10,
+    backgroundColor: '#8fcbbc'
+  },
+  myLogo: {
+    width: 320, 
+    height: 350
+  },
+  cheers: {
+    // display: 'none',
+    width: 320, 
+    height: 250
   }
 })
