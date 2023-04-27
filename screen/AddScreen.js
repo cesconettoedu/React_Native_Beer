dff47963ace8498a83d7d90c57093b3dc99e4c27
edu <cesconettoedu@gmail.com>
2023-04-26 22:37:22 -0400

    instert new beer

--- screen/AddScreen.js
+++ screen/AddScreen.js
@@ -1,23 +1,64 @@
-import React from "react";
-import { SafeAreaView, View, TouchableOpacity, Text, StyleSheet, Image } from "react-native";
+import React, {useState} from "react";
+import { SafeAreaView, View, TouchableOpacity, Text, TextInput, StyleSheet, Image } from "react-native";
 import { useNavigation } from '@react-navigation/native';
+import { supabase } from "../supabase/supabase";
+
 
 
 const AddScreen = () => {
-    
-    const beerSize = 200;
-    const navigation = useNavigation();
+  [newTitle, setNewTitle] = useState('');
+  [newNote, setNewNote] = useState('');
+  
+  const beerSize = 200;
+  const navigation = useNavigation();
+
+
+  const addNewBeer = async () => {
+    console.log(newTitle, newNote);
+    const { data: Beer, error } = await supabase
+    .from('Beer')
+    .insert([
+       { title: newTitle, note: newNote, star:1 },
+     // { newTitle, newNote },
+    ])
+    return Beer
+  }
+
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
    
-      
+                <View style = {styles.container}>
+                      <TextInput style = {styles.input}
+                        underlineColorAndroid = "transparent"
+                        placeholder = " BeerName"
+                        placeholderTextColor = "#9a73ef"
+                        autoCapitalize = "none"
+                        value={newTitle}
+                        //  onChangeText={(e) => setTitle(e.target.value)}/>
+                        onChangeText={setNewTitle}/>
+                      
+                      <TextInput style = {styles.input}
+                        underlineColorAndroid = "transparent"
+                        placeholder = " Note"
+                        placeholderTextColor = "#9a73ef"
+                        autoCapitalize = "none"
+                        value={newNote}
+                        onChangeText={setNewNote}/>
+                        {/* onChangeText={(e) => setNote(e.target.value)}/> */}
+                      
+                      <TouchableOpacity
+                        style = {styles.submitButton}
+                        onPress = {() => addNewBeer()}>
+                        <Text style = {styles.submitButtonText}> Press to Add Beer </Text>
+                      </TouchableOpacity>
+                  </View>
         
-        <Text style={styles.title}>Add Beer Screen</Text>
+        
       
         <TouchableOpacity onPress={() =>  navigation.navigate('ListBeerScreen')} style={styles.close}>
-            <Text style={styles.title}>X close</Text>
+            <Text style={styles.title}>X close Add Beer Screen</Text>
         </TouchableOpacity>
 
       </View>
@@ -29,17 +70,22 @@ export default AddScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "flex-end",
+    paddingTop: 23
   },
-  content: {
-    justifyContent: "center",
-    height: "90%",
-    marginVertical: 20,
-    marginLeft: 8,
-    marginRight: 8,
-    backgroundColor: "rgba(0,0,0,1)",
-    borderRadius: 6,
+  input: {
+      margin: 15,
+      height: 40,
+      borderColor: '#7a42f4',
+      borderWidth: 1
+  },
+  submitButton: {
+      backgroundColor: '#7a42f4',
+      padding: 10,
+      margin: 15,
+      height: 40,
+  },
+  submitButtonText:{
+      color: 'white'
   },
   title: {
     textAlign: "center",
