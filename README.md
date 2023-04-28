4cf03f5b8a7c1d54b51f17307b3f327636b5b41b
edu <cesconettoedu@gmail.com>
2023-04-28 12:49:36 -0400

    adjust imgage preview

--- README.md
+++ README.md
@@ -8,3 +8,69 @@ https://www.youtube.com/watch?v=_uIslLPirw0
 
 backend and database
 https://app.supabase.com/project/ppihnndftvaibkwoaich/api?resource=Beer
+
+
+
+
+<!-- 
+DB Example
+  const data = [
+    {
+    id: 1,
+    title: "Alexander Keiths",
+    imageUrl: "https://cdn.shopify.com/s/files/1/0052/0853/9197/products/full_e48f6bab-351d-4b03-9dbc-a3f19c13c761_580x.jpg?v=1543789208",
+    note: "Ok, i can drink all day",
+    star: 4,
+    mugs: "../assets/05mugs.png"
+    },
+     {
+    id: 2,
+    title: "Creemore Lager",
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShg01AmibJ3TKmfiVXr_ao9VSBvydAVVwlwQ&usqp=CAU",
+    note: "Good",
+    star: 4,
+    mugs: "../assets/05mugs.png"
+    },
+    {
+    id: 3,
+    title: "Blue Moon",
+    imageUrl: "https://www.bluemoonbrewingcompany.com/sites/bluemoon/files/styles/beers/public/beers/2018-06/BlueMoon-BelgianWhite.png?itok=AonO8W6_",
+    note: "Great beer ",
+    star: 4,
+    mugs: "../assets/05mugs.png"
+    },
+    {
+    id: 4,
+    title: "Brahma",
+    imageUrl: "https://www.ambev.com.br/sites/g/files/wnfebl5836/files/styles/webp/public/paragraphs/product_size/2022-09/PRODUTOS_10.png.webp?itok=3K1kJjA0",
+    note: "Can drink, but not a lot",
+    star: 2,
+    mugs: "../assets/05mugs.png"
+    },
+    {
+    id: 5,
+    title: "Budweiser",
+    imageUrl: "https://aem.lcbo.com/content/dam/lcbo/products/9/0/2/6/902619.jpg.thumb.1280.1280.jpg",
+    note: "Light beer, cheap to drink all day",
+    star: 2,
+    mugs: "../assets/05mugs.png"
+    },
+    {
+    id: 6,
+    title: "Coors light",
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvXV4cm80sdH8KK6rqABqxud2bm70o9UOLmg&usqp=CAU",
+    note: "Like water, just if not have other",
+    star: 1,
+    mugs: "../assets/05mugs.png"
+    },
+    {
+    id: 7,
+    title: "Corona",
+    imageUrl: "https://aem.lcbo.com/content/dam/lcbo/products/0/2/2/6/022683.jpg.thumb.1280.1280.jpg",
+    note: "Good , light , can drink all day",
+    star: 4,
+    mugs: "../assets/05mugs.png"
+    },
+   
+  ];
+ -->
--- screen/ListBeerScreen.js
+++ screen/ListBeerScreen.js
@@ -13,71 +13,21 @@ const [single, setSingle] = useState({})
 
 const [beer, setBeer] = useState()
 
-  const data = [
-    {
-    id: 1,
-    title: "Alexander Keiths",
-    imageUrl: "https://cdn.shopify.com/s/files/1/0052/0853/9197/products/full_e48f6bab-351d-4b03-9dbc-a3f19c13c761_580x.jpg?v=1543789208",
-    note: "Ok, i can drink all day",
-    star: 4,
-    mugs: "../assets/05mugs.png"
-    },
-     {
-    id: 2,
-    title: "Creemore Lager",
-    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShg01AmibJ3TKmfiVXr_ao9VSBvydAVVwlwQ&usqp=CAU",
-    note: "Good",
-    star: 4,
-    mugs: "../assets/05mugs.png"
-    },
-    {
-    id: 3,
-    title: "Blue Moon",
-    imageUrl: "https://www.bluemoonbrewingcompany.com/sites/bluemoon/files/styles/beers/public/beers/2018-06/BlueMoon-BelgianWhite.png?itok=AonO8W6_",
-    note: "Great beer ",
-    star: 4,
-    mugs: "../assets/05mugs.png"
-    },
-    {
-    id: 4,
-    title: "Brahma",
-    imageUrl: "https://www.ambev.com.br/sites/g/files/wnfebl5836/files/styles/webp/public/paragraphs/product_size/2022-09/PRODUTOS_10.png.webp?itok=3K1kJjA0",
-    note: "Can drink, but not a lot",
-    star: 2,
-    mugs: "../assets/05mugs.png"
-    },
-    {
-    id: 5,
-    title: "Budweiser",
-    imageUrl: "https://aem.lcbo.com/content/dam/lcbo/products/9/0/2/6/902619.jpg.thumb.1280.1280.jpg",
-    note: "Light beer, cheap to drink all day",
-    star: 2,
-    mugs: "../assets/05mugs.png"
-    },
-    {
-    id: 6,
-    title: "Coors light",
-    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvXV4cm80sdH8KK6rqABqxud2bm70o9UOLmg&usqp=CAU",
-    note: "Like water, just if not have other",
-    star: 1,
-    mugs: "../assets/05mugs.png"
-    },
-    {
-    id: 7,
-    title: "Corona",
-    imageUrl: "https://aem.lcbo.com/content/dam/lcbo/products/0/2/2/6/022683.jpg.thumb.1280.1280.jpg",
-    note: "Good , light , can drink all day",
-    star: 4,
-    mugs: "../assets/05mugs.png"
-    },
-   
-  ];
+//why cant delte data, find where is connceted
+const data ={}
+
 
   //get all beers from supabase
   const getItems = async () => {
       let { data: Beer, error } = await supabase
       .from('Beer')
       .select('*')
+      .order('title') // this have to insert when click o AZ butoon///////////////////////////////////////////////////
+     
+      // when click on List by Star
+      // .order('star',  { ascending: false })
+      // .order('title')
+
       setBeer(Beer)
       return Beer
   }
@@ -130,7 +80,9 @@ const [beer, setBeer] = useState()
       </Modal>
 
 
-      <MenuBottom/>
+      <MenuBottom
+
+      />
 
     </View>
   );
