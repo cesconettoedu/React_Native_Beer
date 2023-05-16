3f5565d41d038ebe0e83362394c2c7dbe27d7113
Eduardo Cesconetto <99515575+cesconettoedu@users.noreply.github.com>
2023-05-16 15:08:07 -0400

    Delete supabase.js

--- supabase/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import 'react-native-url-polyfill/auto'
-
-
-
-const supabaseUrl = 'https://ppihnndftvaibkwoaich.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwaWhubmRmdHZhaWJrd29haWNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIzODc3NTAsImV4cCI6MTk5Nzk2Mzc1MH0.LrNb83wYWGTqg-FtZ6qTAeDZfXBq8sSnYCRluEgilk4'
-export const supabase = createClient(supabaseUrl, supabaseKey)
