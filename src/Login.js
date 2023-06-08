dac79abd017f38b3c8c92afdbff86efc7d3c7030
edu <cesconettoedu@gmail.com>
2023-06-08 12:10:07 -0400

    add cheers gif and name

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { View, StyleSheet, TouchableOpacity, Text, TextInput, Image } from "react-native";
 import { supabase } from "../supabase/supabase";
-import { useIsFocused } from '@react-navigation/native';
-
-import { useNavigation } from '@react-navigation/native';
+import { useIsFocused } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 
 const Login = (props) => {
 
   const navigation = useNavigation();
   const isFocused = useIsFocused();
 
-  const [newUser, setNewUser] = useState('');
-  const [newPass, setNewPass] = useState('');
+  const [newUser, setNewUser] = useState("");
+  const [newPass, setNewPass] = useState("");
   const [showLogo, setShowLogo] = useState(false);
 
   const logCheckUser = async () => {
@@ -20,132 +19,133 @@ const Login = (props) => {
       .select("*")
       .eq("user", newUser)
       .eq("pass", newPass);
-     //  console.log(users[0].id);
+    //  console.log(users[0].id);
     return users;
   };
 
- useEffect(() => {
+  useEffect(() => {
     setTimeout(() => {
-      setShowLogo(false)
-    },1000)
-   
-  },[isFocused]);
+      setShowLogo(false);
+    }, 1000);
+  }, [isFocused]);
 
   return (
     <View>
-
-   
-    {!showLogo &&
-  <View  style={styles.inputUser}>
-              <TextInput
-                style={styles.input}
-                underlineColorAndroid="transparent"
-                placeholder=" Your Name"
-                placeholderTextColor="#4a4e69"
-                autoCapitalize='words'
-                maxLength={30}
-                value={newUser}
-                onChangeText={setNewUser}
-                />
-               <TextInput
-                style={styles.input}
-                underlineColorAndroid="transparent"
-                placeholder=" Password"
-                placeholderTextColor="#4a4e69"
-                secureTextEntry={true}
-                maxLength={30}
-                value={newPass}
-                onChangeText={setNewPass}
-                />
-          <TouchableOpacity onPress={() => {
-              if(newUser === '' || newPass === ''){
-                alert('name and password need to be filled');
+      {!showLogo && (
+        <View style={styles.inputUser}>
+          <TextInput
+            style={styles.input}
+            underlineColorAndroid="transparent"
+            placeholder=" Your Name"
+            placeholderTextColor="#4a4e69"
+            autoCapitalize="words"
+            maxLength={30}
+            value={newUser}
+            onChangeText={setNewUser}
+          />
+          <TextInput
+            style={styles.input}
+            underlineColorAndroid="transparent"
+            placeholder=" Password"
+            placeholderTextColor="#4a4e69"
+            secureTextEntry={true}
+            maxLength={30}
+            value={newPass}
+            onChangeText={setNewPass}
+          />
+          <TouchableOpacity
+            onPress={() => {
+              if (newUser === "" || newPass === "") {
+                alert("name and password need to be filled");
               } else {
-                let userIdExit = 0
-                let userRealId = 0
-                let userName = ''
+                let userIdExit = 0;
+                let userRealId = 0;
+                let userName = "";
 
                 logCheckUser()
-                .then ((user) => {
-                  userIdExit = user.length
-                    if(userIdExit === 1){
-                      userRealId = user[0].id
-                      userName = user[0].user
+                  .then((user) => {
+                    userIdExit = user.length;
+                    if (userIdExit === 1) {
+                      userRealId = user[0].id;
+                      userName = user[0].user;
                     }
-                })
-                .then (() => {
-                  if(userIdExit === 1){
-                  //  alert (`Welcome ${userName}`)
-                  setShowLogo(true)
-                    setTimeout(() => {
-                      navigation.navigate('ListBeerScreen', { id: userRealId })
-                    },3000)
-                  } else {
-                    alert('Wrong user or pass')
-                  }
-                })
+                  })
+                  .then(() => {
+                    if (userIdExit === 1) {
+                      setShowLogo(true);
+                      setTimeout(() => {
+                        navigation.navigate("ListBeerScreen", {id: userRealId,});
+                      }, 2700);
+                    } else {
+                      alert("Wrong user or pass");
+                    }
+                  });
               }
             }}
-             
-             
-            >
-              <Image
-                style={styles.confirm}
-                source={require("../assets/login.png")}
-              />
+          >
+            <Image
+              style={styles.confirm}
+              source={require("../assets/login.png")}
+            />
           </TouchableOpacity>
 
-          <TouchableOpacity
-            onPress={() => props.returnToLogin()}
-            >
+          <TouchableOpacity onPress={() => props.returnToLogin()}>
             <Image
-                style={styles.return}
-                //source={require("../assets/menuBottom/return.png")}
-                source={require("../assets/cameraBtn/cancel.png")}
-              />
-            </TouchableOpacity>
+              style={styles.return}
+              //source={require("../assets/menuBottom/return.png")}
+              source={require("../assets/cameraBtn/cancel.png")}
+            />
+          </TouchableOpacity>
         </View>
+      )
+    }
+      {showLogo && (
+        <View>
+          <Image style={styles.cheers} source={require("../assets/cheers.gif")} />
+          <Text style={styles.cheersName}>Cheers {newUser} !!!</Text>
+        </View>
+      )
       }
-      {showLogo &&
-      <Image
-        style={styles.cheers}
-        source={require('../assets/cheers.gif')}
-      />
-      }
-
-       </View>
+    </View>
   );
 };
 
 export default Login;
 
 const styles = StyleSheet.create({
-  inputUser: {   
-    alignItems: 'center',
+  inputUser: {
+    alignItems: "center",
     justifyContent: "center",
   },
   input: {
-    color: '#7FB069',
-    width: '60%',
+    color: "#7FB069",
+    width: "60%",
     borderRadius: 5,
     margin: 5,
     height: 40,
     borderColor: "#7FB069",
     borderWidth: 1,
-    paddingLeft: 10
+    paddingLeft: 10,
   },
   confirm: {
     marginTop: 20,
-    width: 150, 
-    height: 55
+    width: 150,
+    height: 55,
   },
   return: {
-    width: 55, 
+    width: 55,
     height: 55,
     marginTop: 20,
   },
   cheers: {
     width: 320,
-    height: 250
+    height: 250,
+  },
+  cheersName: {
+
+    textAlign: 'center',
+    color: '#7FB069',
+    fontSize: 23,
+    fontWeight: 600
   }
 });
